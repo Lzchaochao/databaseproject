47118d0be0140c20391a475b85fb80acea3f376f
Wjj <1205131700@qq.com>
2020-04-23 22:40:13 +0800

    wjj提交第一次

---
+++ src/main/java/dbproject/po/StaffDepartmentInformation.java
@@ -0,0 +1,60 @@
+package dbproject.po;
+
+public class StaffDepartmentInformation {
+    private int workNum;
+    private String worksName;
+    private String worksDepartmentName;
+    private int worksLevel;
+    private String workKindName;
+
+    public  getWorkNum() {
+        return workNum;
+    }
+
+    public void setWorkNum(int workNum) {
+        this.workNum = workNum;
+    }
+
+    public String getWorksName() {
+        return worksName;
+    }
+
+    public void setWorksName(String worksName) {
+        this.worksName = worksName;
+    }
+
+    public String getWorksDepartmentName() {
+        return worksDepartmentName;
+    }
+
+    public void setWorksDepartmentName(String worksDepartmentName) {
+        this.worksDepartmentName = worksDepartmentName;
+    }
+
+    public int getWorksLevel() {
+        return worksLevel;
+    }
+
+    public void setWorksLevel(int worksLevel) {
+        this.worksLevel = worksLevel;
+    }
+
+    public String getWorkKindName() {
+        return workKindName;
+    }
+
+    public void setWorkKindName(String workKindName) {
+        this.workKindName = workKindName;
+    }
+
+    @Override
+    public String toString() {
+        return "StaffDepartmentInformation{" +
+                "workNum='" + String.valueOf(workNum) + '\'' +
+                ", worksName='" + worksName + '\'' +
+                ", worksDepartmentName='" + worksDepartmentName + '\'' +
+                ", worksLevel=" + worksLevel +
+                ", workKindName='" + workKindName + '\'' +
+                '}';
+    }
+}
