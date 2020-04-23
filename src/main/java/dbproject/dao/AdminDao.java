294711d57d0cff7e6e70142f2f2731e01132e078
Wjj <1205131700@qq.com>
2020-04-23 23:08:17 +0800

    wjj提交第2次

--- src/main/java/dbproject/dao/AdminDao.java
+++ src/main/java/dbproject/dao/AdminDao.java
@@ -1,7 +1,7 @@
 package dbproject.dao;
-
+import java.util.Date;
 import dbproject.po.Staff;
-
+import dbproject.po.StaffDepartmentInformation;//实现StaffController接收结果创建的类
 import java.util.List;
 
 public interface AdminDao {
@@ -12,4 +12,13 @@ public interface AdminDao {
      * @return 返回list封装
      */
     public List<Staff> getAllStaffInformation();
+    public List <StaffDepartmentInformation> getAllStaffDepartmentInformation();
+
+    public boolean deleteOneBasicStaffInformation(int workNum);
+    public boolean deleteAttdneceInformation(int workNum);
+    public boolean deleteMonthSalaryInformation(int workNum);
+    public boolean deleteOvertimeRecordInformation(int workNum);
+    public boolean deleteYearEndInformation(int workNum);
+
+    public boolean addOneStaffInformation( int WorkNum,Date WorkFromWhen,String WorkerFromWhere,String WorkPwd,int WorksDepartmentId,String WorksIdCard,int WorksLevel,String WorksName,String WorksSex,String WorkTypeId,boolean isWorkState);
 }
